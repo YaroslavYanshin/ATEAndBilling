6a8ea36be1b28048de21ce2d925d7d0940f9f5f9
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-06-04 20:22:53 +0300

    Add classes : Contract, Subscriber,Teriff and interfaces : IContract in Billing

---
+++ ATEAndBilling/Billing/Contract.cs
@@ -0,0 +1,46 @@
+﻿using ATEAndBilling.Enums;
+using ATEAndBilling.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATEAndBilling.Billing
+{
+    public class Contract : IContract
+    {
+        static Random rnd = new Random();
+
+        public Subscriber Subscriber { get; private set; }
+        public int Number { get; private set; }
+        public Tariff Tariff { get; private set; }
+        private DateTime LastTariffUpdateDate;
+
+
+        public Contract(Subscriber subscriber, TariffType tariffType)
+        {
+            LastTariffUpdateDate = DateTime.Now;
+            Subscriber = subscriber;
+            Number = rnd.Next(1000000, 9999999);
+            Tariff = new Tariff(tariffType);
+        }
+
+        public bool ChangeTariff(TariffType tariffType)
+        {
+            if (DateTime.Now.AddMonths(-1) >= LastTariffUpdateDate)
+            {
+                LastTariffUpdateDate = DateTime.Now;
+                Tariff = new Tariff(tariffType);
+                Console.WriteLine("Tariff has changed!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Цait until the end of the month!");
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ATEAndBilling/Interfaces/IContract.cs
@@ -0,0 +1,18 @@
+﻿using ATEAndBilling.Billing;
+using ATEAndBilling.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATEAndBilling.Interfaces
+{
+    public interface IContract
+    {
+        Subscriber Subscriber { get; }
+        int Number { get; }
+        Tariff Tariff { get; }
+        bool ChangeTariff(TariffType tariffType);
+    }
+}
