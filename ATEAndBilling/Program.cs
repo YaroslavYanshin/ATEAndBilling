932195ccf911a2955df1adf4ef2a9aa25e5968e0
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-06-04 19:58:35 +0300

    First commit. Add folders

---
+++ ATEAndBilling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATEAndBilling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
